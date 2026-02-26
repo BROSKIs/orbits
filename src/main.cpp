458aa9585c06e277486f40bbe000de37b944c14f
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-02-26 16:43:07 +0000

    Logic for removing orbiters and added documentation

--- src/main.cpp
+++ src/main.cpp
@@ -13,20 +13,34 @@
 // Important for numerical stability
 static constexpr bn::fixed FORCE_SCALE = 10;
 
+// Maximum number of orbiters allowed on the screen
 static constexpr int MAX_ORBITERS = 30;
 
+// Default starting posiiton and velocity for Orbiter
 static constexpr bn::fixed_point ORBITER_START_POSIITON = {0, 0};
 static constexpr bn::fixed_point ORBITER_START_VELOCITY = {0, 5};
 
-
+/**
+ * A center of mass to be orbited around. Position is controlled by the player.
+ */
 class Center {
     public:
+        /**
+         * Creates a new center.
+         * 
+         * @param starting_position the initial location of the center
+         * @param mass the mass of the center. The larger the value, the greater the attraction
+         * @param speed the speed at which the center moves when the d-pad is held
+         */
         Center(bn::fixed_point starting_position, bn::fixed mass, bn::fixed speed) : 
             _sprite(bn::sprite_items::square.create_sprite(starting_position)),
             _mass(mass),
             _speed(speed) {
         }
 
+        /**
+         * Updates the location of the Center based on player direction with the d-pad.
+         */
         void update() {
             bn::fixed dx = 0;
             bn::fixed dy = 0;
@@ -56,49 +70,81 @@ class Center {
 
     private:
         bn::sprite_ptr _sprite;
-        bn::fixed _mass;
-        bn::fixed _speed;
+        bn::fixed _mass; // the mass of the center. The larger the value, the greater the attraction
+        bn::fixed _speed; //the speed at which the center moves when the d-pad is held
 };
 
+/**
+ * An object that orbits around a center. Orbits using Hooke's law, as if attached by a 2D spring.
+ * The stiffness of the spring in each dimension is center.mass() / FORCE_SCALE. 
+ */
 class Orbiter {
-
     public:
-        Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity) :
+        /**
+         * Creates an orbiter.
+         * 
+         * @param starting_location the initial location of the Orbiter
+         * @param starting_velocity the initial velocity of the Orbiter, {dx, dy}
+         * @param center the center to orbit around
+         */
+        Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity, Center &center) :
         _sprite(bn::sprite_items::dot.create_sprite(starting_location)),
-        _velocity(starting_velocity) {
+        _velocity(starting_velocity),
+        _center(center) {
         }
 
-        void update(Center& center) {
-            bn::fixed_point delta = center.position() - _sprite.position();
-            bn::fixed_point force = delta * center.mass();
-            _velocity += force / FORCE_SCALE;
+        /**
+         * Take a step orbiting around the center.
+         */
+        void update() {
+            // A vector from the Orbiter to the Center
+            bn::fixed_point delta = _center.position() - _sprite.position();
+
+            // Scale the vector by the mass and apply a FORCE_SCALE
+            // This is done in two steps instead of having a pre-scaled mass to prevent loss of
+            // fixed point precision
+            bn::fixed_point force = delta * _center.mass();
+            force /= FORCE_SCALE;
+
+            // Update the current velocity with the force
+            _velocity += force;
+
+            // Update the position by taking a step by the velocity vector
             _sprite.set_position(_sprite.position() + _velocity);
         }
 
     private:
         bn::sprite_ptr _sprite;
         bn::fixed_point _velocity;
+        Center& _center;
 };
 
 int main() {
     bn::core::init();
-    Center center = Center({30, 40}, .05, 2);
 
+    Center center = Center({30, 40}, .05, 2);
     bn::vector<Orbiter, MAX_ORBITERS> orbiters = {};
     
-
     while(true) {
+        // Add an orbiter when A is pressed if there's room
         if(bn::keypad::a_pressed()) {
             if(orbiters.size() < MAX_ORBITERS) {
-                orbiters.push_back(Orbiter(ORBITER_START_POSIITON, ORBITER_START_VELOCITY));
+                orbiters.push_back(Orbiter(ORBITER_START_POSIITON, ORBITER_START_VELOCITY, center));
+            }
+        }
+
+        // Remove an orbiter when B is pressed if there's at least one
+        if (bn::keypad::b_pressed()) {
+            if(orbiters.size() > 0) {
+                orbiters.pop_back();
             }
         }
 
         center.update();
-
         for(Orbiter& orbiter : orbiters) {
-            orbiter.update(center);
+            orbiter.update();
         }
+        
         bn::core::update();
     }
 }
