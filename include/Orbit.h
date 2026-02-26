6c12f8a3c6e16c1e0532332dc56b523d8105d6a7
Pasha <pavloyousee@gmail.com>
2026-02-26 19:51:50 +0000

    Wave 4 compleate

---
+++ include/Orbit.h
@@ -0,0 +1,41 @@
+#ifndef ORBITER_H
+#define ORBITER_H
+#include <bn_fixed_point.h>
+
+#include "Center.h"
+
+// A scaling factor by which to reduce the force applied when orbiting
+// Important for numerical stability
+static constexpr bn::fixed FORCE_SCALE = 10;
+
+// Default starting posiiton and velocity for Orbiter
+static constexpr bn::fixed_point ORBITER_START_POSIITON = {0, 0};
+static constexpr bn::fixed_point ORBITER_START_VELOCITY = {0, 5};
+
+/**
+ * An object that orbits around a center. Orbits using Hooke's law, as if attached by a 2D spring.
+ * The stiffness of the spring in each dimension is center.mass() / FORCE_SCALE.
+ */
+class Orbiter
+{
+public:
+    /**
+     * Creates an orbiter.
+     *
+     * @param starting_location the initial location of the Orbiter
+     * @param starting_velocity the initial velocity of the Orbiter, {dx, dy}
+     * @param center the center to orbit around
+     */
+    Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity, Center &center);
+
+    /**
+     * Take a step orbiting around the center.
+     */
+    void update();
+
+private:
+    bn::sprite_ptr _sprite;
+    bn::fixed_point _velocity;
+    Center &_center;
+};
+#endif
